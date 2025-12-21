011ecac199e7ac49bb57c7eb39fb7fe4f8618304
Eli112358 <4665184+Eli112358@users.noreply.github.com>
2025-12-21 04:18:57 -0500

    Fix `config` borrowing

--- ed-colonization/src/main.rs
+++ ed-colonization/src/main.rs
@@ -6,18 +6,18 @@ const APP_NAME: &str = "ed-colonization";
 
 fn main() {
     let mut config = Config::new();
-    let journal_location = &config.data.journal_location;
+    let journal_location = String::from(&config.data.journal_location);
     println!("[debug] Config file: {}", &config.file.display());
     println!("Journal Location: {}", journal_location);
-    println!("Minimum stock: {}", config.data.minimum_stock);
+    println!("Minimum stock: {}", &config.data.minimum_stock);
     println!("Tracked:");
     let tracked_constructions = &config.data.tracked_constructions;
     for tracked_construction in tracked_constructions {
         println!("  {}", tracked_construction);
     }
-    println!("List all available: {}", config.data.list_all);
-    println!("Show totals: {}", config.data.show_totals);
-    println!("Totals Only: {}", config.data.totals_only);
+    println!("List all available: {}", &config.data.list_all);
+    println!("Show totals: {}", &config.data.show_totals);
+    println!("Totals Only: {}", &config.data.totals_only);
 
     // for testing `config.save` and `config.reload` and avoid `never used` warnings
     config.data.totals_only = !config.data.totals_only;
